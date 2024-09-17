182964f2f116471f7f1c0a4961180f37414340b1
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-17 14:02:57 -0300

    create ResourceNotFoundException

---
+++ src/main/java/com/tidz/proto/goodgym/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tidz.proto.goodgym.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+}
