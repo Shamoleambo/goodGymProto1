0a9956da0e4eb08ff8ab11d41d59d286ebc9db5d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-23 11:27:05 -0300

    create ExerciseRepository interface

---
+++ src/main/java/com/tidz/proto/goodgym/repository/ExerciseRepository.java
@@ -0,0 +1,9 @@
+package com.tidz.proto.goodgym.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tidz.proto.goodgym.model.Exercise;
+
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+}
