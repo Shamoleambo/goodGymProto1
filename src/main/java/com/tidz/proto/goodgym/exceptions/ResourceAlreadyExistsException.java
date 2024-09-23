3a3a44c19572853e673821f20b16cad8910a837a
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-23 15:11:02 -0300

    create a custom error for when an exercise already exist, for the name of the exercise must be unique

---
+++ src/main/java/com/tidz/proto/goodgym/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.tidz.proto.goodgym.exceptions;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+
+	public ResourceAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
