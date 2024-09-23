75e75991e2b007a2947f70e1aa49def1c4d0143e
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-23 11:07:12 -0300

    create new Exercise entity which is more generic than the previous one

---
+++ src/main/java/com/tidz/proto/goodgym/model/Exercise.java
@@ -0,0 +1,51 @@
+package com.tidz.proto.goodgym.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "exercises")
+public class Exercise {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+	@Column(name = "body_area")
+	@Enumerated(EnumType.STRING)
+	private BodyArea bodyArea;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BodyArea getBodyArea() {
+		return bodyArea;
+	}
+
+	public void setBodyArea(BodyArea bodyArea) {
+		this.bodyArea = bodyArea;
+	}
+
+}
