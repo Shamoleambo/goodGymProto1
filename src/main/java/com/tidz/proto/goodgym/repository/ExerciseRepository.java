eea2c501d2d3dac8db70c9bc6e37526fa9269504
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-23 15:11:36 -0300

    add new method to findByName in ExerciseRepository

--- src/main/java/com/tidz/proto/goodgym/repository/ExerciseRepository.java
+++ src/main/java/com/tidz/proto/goodgym/repository/ExerciseRepository.java
@@ -1,9 +1,13 @@
 package com.tidz.proto.goodgym.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tidz.proto.goodgym.model.Exercise;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+	Exercise findByName(String name);
 
 }
