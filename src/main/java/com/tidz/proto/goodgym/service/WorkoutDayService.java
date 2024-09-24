d6798bddc35fdf94e96dc88915d642f7ff9402dc
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-24 15:14:02 -0300

    refactor the WorkoutDay service to save the exercises along with the routines and days

--- src/main/java/com/tidz/proto/goodgym/service/WorkoutDayService.java
+++ src/main/java/com/tidz/proto/goodgym/service/WorkoutDayService.java
@@ -1,8 +1,13 @@
 package com.tidz.proto.goodgym.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.tidz.proto.goodgym.model.Exercise;
+import com.tidz.proto.goodgym.model.ExerciseRoutine;
 import com.tidz.proto.goodgym.model.WorkoutDay;
+import com.tidz.proto.goodgym.repository.ExerciseRepository;
 import com.tidz.proto.goodgym.repository.WorkoutDayRepository;
 
 import jakarta.transaction.Transactional;
@@ -11,13 +16,24 @@ import jakarta.transaction.Transactional;
 public class WorkoutDayService {
 
 	private final WorkoutDayRepository workoutDayRepository;
+	private final ExerciseRepository exerciseRepository;
 
-	public WorkoutDayService(WorkoutDayRepository dayRepository) {
+	public WorkoutDayService(WorkoutDayRepository dayRepository, ExerciseRepository exerciseRepository) {
 		this.workoutDayRepository = dayRepository;
+		this.exerciseRepository = exerciseRepository;
 	}
 
 	@Transactional
 	public WorkoutDay save(WorkoutDay day) {
+		for (ExerciseRoutine routine : day.getWorkout()) {
+			Exercise exercise = Optional.ofNullable(exerciseRepository.findByName(routine.getExercise().getName()))
+					.orElseGet(() -> {
+						Exercise newExercise = new Exercise(routine.getExercise().getName(),
+								routine.getExercise().getBodyArea());
+						return exerciseRepository.save(newExercise);
+					});
+			routine.setExercise(exercise);
+		}
 		return workoutDayRepository.save(day);
 	}
 
