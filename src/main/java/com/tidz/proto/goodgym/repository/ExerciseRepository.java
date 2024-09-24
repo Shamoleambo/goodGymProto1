70565df63729de76bb64c6c39963f4152f8b2420
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-24 12:02:26 -0300

    remove an unused import in the exercise repository

--- src/main/java/com/tidz/proto/goodgym/repository/ExerciseRepository.java
+++ src/main/java/com/tidz/proto/goodgym/repository/ExerciseRepository.java
@@ -1,7 +1,5 @@
 package com.tidz.proto.goodgym.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tidz.proto.goodgym.model.Exercise;
